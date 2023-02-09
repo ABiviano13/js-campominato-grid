7c5a72a5229b19b7b0e91c2b3b482d79b2fcc8af
ABiviano13 <abiviano0@outlook.com>
2023-02-09 15:36:00 +0100

    START

---
+++ js/app.js
@@ -0,0 +1,6 @@
+/// FUNZIONI ///
+
+
+// CREAZIONE GRIGLIA
+//1. Creazione della griglia (essa verrà generata al click sul bottone)
+//2. Al click su una cella si deve stampare il numero relativo alla cella cliccata sulla console
